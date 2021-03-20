be29c55b85b76e6d4252bc7847df8d2bdc9ccd36
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-03-20 23:48:44 +0700

    add test for rename Interop\Container\ContainerInterface to \Psr\Container\ContainerInterface in invoke type hint
    Signed-off-by: Abdul Malik Ikhsan <samsonasik@gmail.com>
    
    Si# gned-off-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

---
+++ test/RenameClassRector/Source/Service.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasTest\ServiceManager\Migration\RenameClassRector\Source;
+
+class Service
+{
+    public function __construct($adapter)
+    {
+
+    }
+}
